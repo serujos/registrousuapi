68ccbc5de2d8e98f0370b2e74502a621bb2d4264
jos <josmerjauriacaceria@gmail.com>
2025-05-23 10:27:37 -0500

    proyecto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace registrousuapi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
